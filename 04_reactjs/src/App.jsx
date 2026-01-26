6c36f9de59b5e57f1f5571f117b1616ba0797a4f
wael <78425538+wa20@users.noreply.github.com>
2026-01-26 07:09:35 +0000

    vite amends and component set up

--- 04_reactjs/src/App.jsx
+++ 04_reactjs/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Car from './component/Car'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, useEffect } from 'react'
 
+
+const App = () => {
+
+  const [cars, setCars] = useState([])
+
+  useEffect(() => { 
+    fetch('api/v1/cars')
+    .then( res => res.json())
+    .then( data => setCars(data))
+    .catch( err => console.log(err))
+
+    
+  },[cars])
+
+  console.log(cars)
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Welcome to the car store</h1>
+      <ul>
+        < Car />
+      </ul>
+    </div>
   )
 }
 
 export default App
+ 
